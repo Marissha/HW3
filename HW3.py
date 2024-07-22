e825bf7ea31f5bfd25ca4dc5cd2765b81c1b5368
Marissha <balabobalaba71@gmail.com>
2024-07-22 13:36:20 +0300

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,8 @@
+name= 'Marina'
+print('Name: ' + str(name))
+age = 25
+print('Age: ' + str(age))
+age = age + 1
+print('New age: ' + str(age))
+is_student = True
+print('Is student: ' + str(is_student))
